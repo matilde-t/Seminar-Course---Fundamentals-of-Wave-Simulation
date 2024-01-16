36789280b5bcced1acd57b078d719f5436feaff1
Matilde Tozzi <ge83bug@tum.de>
2024-01-16 17:44:56 +0100

    start presentation layout

---
+++ clean.sh
@@ -0,0 +1,12 @@
+find . -type f -name '*.aux' -delete
+find . -type f -name '*.fdb*' -delete
+find . -type f -name '*.fls' -delete
+find . -type f -name '*.log' -delete
+find . -type f -name '*.sync*' -delete
+find . -type f -name '*.toc' -delete
+find . -type f -name '*.snm' -delete
+find . -type f -name '*.nav' -delete
+find . -type f -name '*.out' -delete
+find . -type f -name '*.bak0' -delete
+find . -type f -name '*.vrb' -delete
+
