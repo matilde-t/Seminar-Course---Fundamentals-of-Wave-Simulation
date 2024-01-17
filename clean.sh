9498230230f5a9e2402d6573c6155ed96fe03587
Matilde Tozzi <ge83bug@tum.de>
2024-01-18 00:53:59 +0100

    unsplit method

--- clean.sh
+++ clean.sh
@@ -7,6 +7,6 @@ find . -type f -name '*.toc' -delete
 find . -type f -name '*.snm' -delete
 find . -type f -name '*.nav' -delete
 find . -type f -name '*.out' -delete
-find . -type f -name '*.bak0' -delete
+find . -type f -name '*.bak*' -delete
 find . -type f -name '*.vrb' -delete
 
